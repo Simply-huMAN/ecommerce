98cfbd0bff07393ac57cf70069c0d3f4a658702a
Aditya <malothaditya006@gmail.com>
2025-03-02 20:45:58 +0530

    Backend: remove unused imports in UserService, in the previous `Backend` titled commit message the changes are: add static convertToAuthDTO, convertToDTO in respective classes, add withdraw and recharge methods in Wallet class

--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -7,10 +7,7 @@ import com.example.ecommerce.repository.UserRepository;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @Service
